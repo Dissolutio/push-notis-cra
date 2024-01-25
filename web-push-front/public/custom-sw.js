e372eb615e1869fb531d19e59cfdd09a72e2f7d4
John M <entity.john@gmail.com>
2024-01-24 20:44:55 -0600

    remove work-around

--- web-push-front/public/custom-sw.js
+++ web-push-front/public/custom-sw.js
@@ -1,13 +1,8 @@
 self.addEventListener("push", (event) => {
-  const data = { body: "default body" };
-  try {
-    const data = event?.data?.json();
-  } catch (error) {
-    console.log("🚀 ~ self.addEventListener ~ error:", error);
-  }
+  const data = event.data.json();
   console.log("New notification", data);
   const options = {
-    body: data?.body,
+    body: data.body,
   };
   event.waitUntil(self.registration.showNotification(data.title, options));
 });
